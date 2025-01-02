6737e5bd147d3db378354abf7fd936396f69f6ba
defaaryawar <154545695+defaaryawar@users.noreply.github.com>
2025-01-02 21:21:17 +0700

    Update CardFilm.jsx

--- src/components/CardFilm/CardFilm.jsx
+++ src/components/CardFilm/CardFilm.jsx
@@ -80,7 +80,7 @@ const CardFilm = ({ head, title, item, category }) => {
     <div id="main-content" className="md:pb-10 md:px-28 sm:pb-15 sm:px-15 lg:pb-25 md:px-25 px-4 pb-3">
       <div className="flex justify-between">
         <p className="text-white text-lg sm:2xl md:3xl lg:3xl font-bold">{title}</p>
-        <div className="relative flex gap-4 items-center md:px-10 px-0">
+        <div className="relative flex gap-4 items-center md:px-10 px-2">
           <MenuCard
             title={selectedMenu2}
             items={menu2Items}
