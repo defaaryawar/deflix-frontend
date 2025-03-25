1a1371cc9b1651b65d8c3c302b8153c43db96fec
defaaryawar1 <defadefa1313@gmail.com>
2025-03-25 18:40:21 +0700

    perbaikan pada hit ke api benya

--- src/app/otp/page.jsx
+++ src/app/otp/page.jsx
@@ -94,7 +94,7 @@ const OtpPage = () => {
             setIsLoading(true);
             const email = getEmail();
 
-            const response = await fetch("https://deflix-backend-production.up.railway.app/api/users", {
+            const response = await fetch("https://naughty-emogene-deflix-67da416e.koyeb.app/api/users", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -108,7 +108,7 @@ const OtpPage = () => {
                 throw new Error(data.message || "Gagal mendaftar. Silakan coba lagi.");
             }
     
-            const tokenResponse = await fetch("https://deflix-backend-production.up.railway.app/api/auth/login", {
+            const tokenResponse = await fetch("https://naughty-emogene-deflix-67da416e.koyeb.app/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
